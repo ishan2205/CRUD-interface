330dc7fa4adfbc1c853743d20a815b6819979184
anujkap <anujkapoor10@gmail.com>
2019-08-26 16:45:32 +0530

    ScriptEditorStart

---
+++ app/src/main/java/com/anuj/crud_interface/ScriptEditor.java
@@ -0,0 +1,14 @@
+package com.anuj.crud_interface;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScriptEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.script_editor);
+    }
+}
