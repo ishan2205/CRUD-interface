1ac787ede8ff07590e036e8e15f2f8e81ec9c784
anujkap <anujkapoor10@gmail.com>
2019-09-02 23:59:06 +0530

    Login/Sign Up Page Script Editor General Flow of App

--- app/src/main/java/com/anuj/crud_interface/MainActivity.java
+++ app/src/main/java/com/anuj/crud_interface/MainActivity.java
@@ -1,85 +1,40 @@
 package com.anuj.crud_interface;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.Toast;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    List<Table> tableList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        ActionBar actionBar=getSupportActionBar();
+        /*ActionBar actionBar=getSupportActionBar();
         assert actionBar != null;
-        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFFF6666));
-        tableList= new ArrayList<>();
-        recyclerView=findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        tableList.add(new Table(1,"Table 1","Hello")
-        );
-        tableList.add(new Table(2,"Table 2","World")
-        );
-        TableAdapter adapter=new TableAdapter(this,tableList);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                Toast.makeText(getApplicationContext(),"HI",Toast.LENGTH_LONG).show();
-            }
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFF2554F9));
+        actionBar.setTitle("MongoDB Login");
 
-            @Override
-            public void onLongItemClick(View view, int position) {
-                Toast.makeText(getApplicationContext(),"SUP",Toast.LENGTH_LONG).show();
-            }
-        }));
+         */
 
-        /*MongoClientURI uri = new MongoClientURI(
-                "mongodb://anujkap:<password>@testcluster-shard-00-00-6tiz2.mongodb.net:27017,testcluster-shard-00-01-6tiz2.mongodb.net:27017,testcluster-shard-00-02-6tiz2.mongodb.net:27017/test?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin&retryWrites=true&w=majority");
+        Button button=findViewById(R.id.login_button);
+        button.setOnClickListener(view -> {
+            //TODO:Send Login Info and get Acknowledgement if Success then proceed
+            Intent intent=new Intent(getApplicationContext(),ScriptEditor.class);
+            startActivity(intent);
+        });
+        //TODO:Link Sign Up button
 
-        MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("test");
-    */
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.mybutton) {
-            //Link Retrofit Here
-            Toast.makeText(getApplicationContext(),"ADDED",Toast.LENGTH_LONG).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/anuj/crud_interface/ScriptEditor.java
+++ app/src/main/java/com/anuj/crud_interface/ScriptEditor.java
@@ -1,14 +1,67 @@
 package com.anuj.crud_interface;
 
+import android.app.AlertDialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScriptEditor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.script_editor);
+        ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xff242431));
+        actionBar.setTitle("Console");
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.script_editor, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.submit) {
+            //TODO:Link API Here, send instruction to API and get the data along with acknowledgement
+            Toast.makeText(getApplicationContext(),"RUN",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onBackPressed()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ScriptEditor.this);
+        builder.setTitle("Are you sure you want to Log Out?");
+        builder.setMessage("Test");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            //TODO:Link Page for Logout
+            finish();
+        });
+
+        builder.setNegativeButton("No", (dialog, which) -> {
+            dialog.cancel();
+        });
+
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+
+    }
+
 }
